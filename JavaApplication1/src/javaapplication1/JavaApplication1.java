61c1cfd52ce7421b71bd8353182de4a64e9111a8
maurizio.franchi <maurizio.franchi@AULA75-PC07.itib.local>
2021-11-23 15:24:19 +0100

    Preparazione test

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -6,7 +6,14 @@ public class JavaApplication1 {
 
    
     public static void main(String[] args) {
-        System.out.println("Ciao");
+        
+        System.out.println("Il primo addendo è: ");
+        
+        System.out.println("Il secondo addendo è: ");
+        
+        System.out.println("La somma è: ");
+        
+        
     }
     
 }
